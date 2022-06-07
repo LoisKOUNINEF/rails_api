f1ec629802d5dcbea032bece08a76b556bedd0dd
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-07 15:16:33 +0200

    link user to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
+
+  has_many :articles
 end
